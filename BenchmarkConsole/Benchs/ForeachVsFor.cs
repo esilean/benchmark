bc01fd615ce324eafe31df030de9fa03d84dbedd
Leandro Bevilaqua <le.bevilaqua@gmail.com>
2021-07-11 15:28:20 -0300

    sdt

---
+++ BenchmarkConsole/Benchs/ForeachVsFor.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace BenchmarkConsole.Benchs
+{
+    public class ForeachVsFor
+    {
+
+        [Benchmark]
+        public int GetAllBanksForEach()
+        {
+            var banks = GetBanks();
+            var total = 0;
+            foreach (var b in banks)
+            {
+                total += b.Number;
+            }
+
+            return total;
+        }
+
+        [Benchmark]
+        public int GetAllBanksFor()
+        {
+            var banks = GetBanks();
+            var total = 0;
+            for (int i = 0; i < banks.Count; i++)
+            {
+                total += banks[i].Number;
+            }
+
+            return total;
+        }
+
+        private List<Bank> GetBanks()
+        {
+            //var newList = new List<Bank>();
+            var newList = new List<Bank>(1_000_000);
+            for (int i = 0; i < 1_000_000; i++)
+                newList.Add(new Bank { Number = i });
+
+            return newList;
+        }
+
+    }
+
+    public class Bank
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ BenchmarkConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkConsole.Benchs;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ForeachVsFor>();
+        }
+    }
+}
